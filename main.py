99de3e173686477c30971eda150e8516f2731c40
nazar814 <nazarzaloha2@gmail.com>
2024-09-24 23:47:51 +0200

    port updated

--- main.py
+++ main.py
@@ -81,5 +81,5 @@ def process_video():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))  # Default to port 5000 if PORT is not set
+    port = int(os.getenv('PORT', 10000))  # Default to port 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port, debug=True)

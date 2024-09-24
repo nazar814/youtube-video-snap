b23619781555f56cbd93e7b9660c130484897798
nazar814 <nazarzaloha2@gmail.com>
2024-09-24 23:15:46 +0200

    port updated

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ def process_video():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))  # Default to port 5000 if PORT is not set
-    app.run(host='0.0.0.0', port=port, debug=True)app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=port, debug=True)
